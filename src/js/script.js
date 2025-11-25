783e707174e5aef8d9c876d97a86533120fda754
Antonio Malaquias <antoniomarcosmalaquias@hotmail.com>
2025-11-24 21:11:47 -0300

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,12 @@ const borda = document.getElementById('borda');
 const user = document.getElementById('user');
 const login = document.getElementById('login');
 const entrar = document.getElementById('entrar');
+const senha = document.getElementById('senha');
+const errou = document.getElementById('errou');
+const cliente = document.getElementById('cliente');
+const servico = document.getElementById('servico');
+const profi = document.getElementById('profi');
+const agendamentos = document.getElementById('agendamentos');
 
 borda.addEventListener('mouseover',()=>{
     borda.style.backgroundColor = 'white';
@@ -16,12 +22,19 @@ borda.addEventListener('mouseout',()=>{
 borda.addEventListener('click',()=>{
     if(login.style.visibility == 'visible'){
         login.style.visibility = 'hidden';
+        errou.style.visibility ='hidden';
         return;
     }
     login.style.visibility = 'visible';
     return;
 });
 
+
+
+
 function logar(){
-    
+    console.log(senha.value)
+    if(!senha.value){
+        errou.style.visibility ='visible';
+    }
 }
